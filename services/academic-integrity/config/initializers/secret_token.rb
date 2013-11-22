e5d5ffc7e5ef5199e602f2f7544db78c657b54a5
Phil McGachey <phil_mcgachey@harvard.edu>
2013-11-22 09:24:14 -0500

    Created project skeleton for academic integrity policy wizard

---
+++ services/academic-integrity/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AcademicIntegrity::Application.config.secret_key_base = '95345ea17cb91ca4c49298c6ed068e3a3a39a3a606ef98577985a55d0d57d037963c50b49b94aab622d96036ad72302bbdf76c5346c17476203afb8d76d8d470'
